11db109a2a919ed45defa7410261d2daeca2042b
Pierre Cariou <pierrecariou@outlook.fr>
2019-02-13 15:41:40 +0100

    y

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,43 @@
+class RestaurantsController < ApplicationController
+#            Prefix Verb   URI Pattern                     Controller#Action
+#     restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
+before_action :set_restaurant, only: [:show]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def restaurant_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'restau',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 75 64 25 88',
+    category:      'chinese'
+  },
+    {
+    name:         'Dishoo',
+    address:      '7 Boundary St, London E2 7J',
+    phone_number:  '06 75 64 25 8',
+    category:      'chinese'
+  },
+    {
+    name:         'Disho',
+    address:      '7 Boundary St, London E2 7',
+    phone_number:  '06 75 64 25 ',
+    category:      'chinese'
+  },
+    {
+    name:         'Dis',
+    address:      '7 Boundary St, London E2 ',
+    phone_number:  '06 75 64 2',
+    category:      'chinese'
+  },
+    {
+    name:         'D',
+    address:      '7 Boundary St, London ',
+    phone_number:  '06 75 6',
+    category:      'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
